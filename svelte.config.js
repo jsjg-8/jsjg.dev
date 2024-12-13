2e10c11f2bbb903ec46e271532bfb99b18a31b8e
jsjg-8 <jsjggroups@gmail.com>
2024-12-13 11:41:04 -0500

    update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { mdsvex } from "mdsvex";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('mdsvex').MdsvexOptions} */
@@ -17,15 +17,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adbapters for more information about adapters.
-    adapter: adapter({
-      // default options are shown. On some platforms
-      // these options are set automatically — see below
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: true,
-      strict: false
-    })
+    adapter: adapter()
   },
 
   extensions: [".svelte", ".svx"],
